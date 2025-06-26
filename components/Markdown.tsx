624385b13a4e0df7bc4f0b4c2169b18ce4b1964e
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2025-06-26 14:04:25 -0300

    feat: adjust errors in markdown rendering

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -6,20 +6,23 @@ export default function MyMarkdown(props: MarkdownProps & {children: string}) {
     const textColor = useThemeColor("text")
     const codeInlineColor = useThemeColor("codeInlineBackground")
 
+    const textContentStyle = {flexDirection: 'row', flexWrap: 'wrap', lineHeight: 30}
+
     return (
         <Markdown
             style={{
                 body: { fontSize: 15, color: textColor, fontFamily: 'Roboto' },
-                //text: {fontFamily: 'Roboto', flexDirection: 'row', flexWrap: 'wrap'},
+                text: {verticalAlign: 'middle'},
                 image: {
                     maxWidth: 150,
-                    maxHeight: 150
+                    maxHeight: 150,
+                    marginBottom: -5
                 },
                 ordered_list: {flexDirection: 'column', flexWrap: 'wrap', width: "100%"},
-                ordered_list_content: {flexDirection: 'row', flexWrap: 'wrap'},
                 bullet_list: {flexDirection: 'column', flexWrap: 'wrap', width: "100%"},
-                bullet_list_content: {flexDirection: 'row', flexWrap: 'wrap'},
-                paragraph: {flexDirection: 'row', flexWrap: 'wrap'},
+                ordered_list_content: {flexDirection: 'row', flexWrap: 'wrap', lineHeight: 30},
+                bullet_list_content: {flexDirection: 'row', flexWrap: 'wrap', lineHeight: 30},
+                paragraph: {flexDirection: 'row', flexWrap: 'wrap', lineHeight: 30},
                 textgroup: {flexDirection: 'row', flexWrap: 'wrap'},
                 code_inline: { backgroundColor: codeInlineColor}
             }}
