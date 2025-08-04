2d85aa1b2f967d2b3280a67876fcca51fed595f4
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2025-08-04 12:10:11 -0300

    chore: bump version code

--- app.config.ts
+++ app.config.ts
@@ -35,7 +35,7 @@ export default {
   expo: {
     name: AppVersion.name,
     slug: "rag-chat",
-    version: "1.0.0",
+    version: "1.2.0",
     orientation: "portrait",
     icon: AppVersion.icon,
     scheme: "ragchat",
